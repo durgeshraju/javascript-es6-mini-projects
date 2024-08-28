434ea92a6158c5f9ef39a7a0ee5517464aef0feb
ndurgesh88 <durgeshnidadavolu@gmail.com>
2024-08-28 18:03:19 +0800

    Update task input validation and submission functionality

---
+++ 01.LaunchPad-TodoList-App/scripts/app.js
@@ -0,0 +1,19 @@
+const taskInput = document.querySelector('.task-input');
+const addTask = document.querySelector('.add-task');
+const taskForm = document.querySelector('#taskForm');
+
+const addTaskInput = (e) => {
+  e.target.value.trim().length > 0
+    ? addTask.removeAttribute('disabled')
+    : addTask.setAttribute('disabled', true);
+};
+taskInput.addEventListener('keyup', addTaskInput);
+
+const btnAddTaskHandler = (e) => {
+  console.log(e);
+  e.preventDefault();
+  const task = taskInput.value.trim();
+  console.log('Add Task:', task);
+  taskForm.reset();
+};
+addTask.addEventListener('click', btnAddTaskHandler);
