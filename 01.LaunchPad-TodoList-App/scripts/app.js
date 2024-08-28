3707354521698ccc0dff3ee4e7e218bc195ba8af
ndurgesh88 <durgeshnidadavolu@gmail.com>
2024-08-28 18:14:55 +0800

    adding commenting to the code

--- 01.LaunchPad-TodoList-App/scripts/app.js
+++ 01.LaunchPad-TodoList-App/scripts/app.js
@@ -1,13 +1,19 @@
 const taskInput = document.querySelector('.task-input');
 const addTask = document.querySelector('.add-task');
 const taskForm = document.querySelector('#taskForm');
+const taskList = document.querySelector('.task-list');
 
+// Add task input event handler
 const addTaskInput = (e) => {
   e.target.value.trim().length > 0
     ? addTask.removeAttribute('disabled')
     : addTask.setAttribute('disabled', true);
 };
+
+// Add task input event listener
 taskInput.addEventListener('keyup', addTaskInput);
+
+// Add task button event handler
 
 const btnAddTaskHandler = (e) => {
   console.log(e);
@@ -17,4 +23,6 @@ const btnAddTaskHandler = (e) => {
   taskForm.reset();
   addTask.setAttribute('disabled', true);
 };
+
+// Add task button event listener
 addTask.addEventListener('click', btnAddTaskHandler);
