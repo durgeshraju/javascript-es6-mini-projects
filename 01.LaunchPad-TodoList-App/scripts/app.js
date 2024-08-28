3b5d9f1633347de384c215167ef6e5d247402b21
ndurgesh88 <durgeshnidadavolu@gmail.com>
2024-08-28 18:50:00 +0800

    Render tasks dynamically in task list

--- 01.LaunchPad-TodoList-App/scripts/app.js
+++ 01.LaunchPad-TodoList-App/scripts/app.js
@@ -2,6 +2,28 @@ const taskInput = document.querySelector('.task-input');
 const addTask = document.querySelector('.add-task');
 const taskForm = document.querySelector('#taskForm');
 const taskList = document.querySelector('.task-list');
+const taskItem = document.querySelector('.task-item');
+
+// Task list
+
+const tasks = [];
+console.log('Task List:', tasks);
+
+// Render tasks
+const renderTasks = () => {
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    taskList.innerHTML += `
+      <li class="task-item">
+        <input type="checkbox" class="task-checkbox" />
+        <p class="task-text">${task}</p>
+        <a href="#" class="delete-task" data-index="${index}">
+          <i class="far fa-trash-alt"></i>
+        </a>
+      </li>
+    `;
+  });
+};
 
 // Add task input event handler
 const addTaskInput = (e) => {
@@ -16,10 +38,9 @@ taskInput.addEventListener('keyup', addTaskInput);
 // Add task button event handler
 
 const btnAddTaskHandler = (e) => {
-  console.log(e);
   e.preventDefault();
   const task = taskInput.value.trim();
-  console.log('Add Task:', task);
+  renderTasks(tasks.push(task));
   taskForm.reset();
   addTask.setAttribute('disabled', true);
 };
