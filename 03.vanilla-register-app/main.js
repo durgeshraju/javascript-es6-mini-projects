20d46ec32b885c5907ec6f1399bc4fb21c1d162e
ndurgesh88 <durgeshnidadavolu@gmail.com>
2025-08-09 23:00:12 +0800

    removing previous imports..

--- 03.vanilla-register-app/main.js
+++ 03.vanilla-register-app/main.js
@@ -1,1 +1,6 @@
 // App ← Entry point
+// import initForm from './components/form.js';
+
+// const appRoot = document.getElementById('appRoot');
+// const formElement = initForm(); // This returns the <form> element
+// appRoot.appendChild(formElement);
